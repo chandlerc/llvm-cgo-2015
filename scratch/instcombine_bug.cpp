a67a1752446ed7a7105e3df2c2ba2a57d17782c2
Chandler Carruth <chandlerc@gmail.com>
2015-02-07 00:52:47 -0800

    Switch to static rather than inline, the test case is better that way.

--- scratch/instcombine_bug.cpp
+++ scratch/instcombine_bug.cpp
@@ -1,17 +1,17 @@
 #include <cstring>
 
-inline unsigned int h(float value) {
+static unsigned int h(float value) {
   unsigned int i;
   memcpy(&i, &value, sizeof(value));
   return i;
 }
 
-inline unsigned char *write(unsigned int v, unsigned char *b) {
+static unsigned char *write(unsigned int v, unsigned char *b) {
   memcpy(b, &v, sizeof(v));
   return b + sizeof(v);
 }
 
-inline unsigned char *write2(float value, unsigned char *b) {
+static unsigned char *write2(float value, unsigned char *b) {
   return write(h(value), b);
 }
 
